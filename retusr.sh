946a2c0f45e2deb71e72e6038456620bbf0abc18
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-26 19:44:37 +0900

    Functionized

--- retusr.sh
+++ retusr.sh
@@ -1,2 +1,12 @@
 #! /bin/sh
-cat ./target/sshlog.txt | awk 'match($0, /user.*from/){print substr($0, RSTART+5, RLENGTH-10)}'
+# cat ./target/sshlog.txt | awk 'match($0, /user.*from/){print substr($0, RSTART+5, RLENGTH-10)}'
+
+_retrieve_username () {
+    awk 'match($0, /user.*from/){print substr($0, RSTART+5, RLENGTH-10)}' $(cat -)
+}
+
+if [ -p /dev/stdin ]; then
+    cat -
+else
+    echo $@
+fi | _retrieve_username
