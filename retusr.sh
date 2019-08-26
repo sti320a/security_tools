1f87eea942e9d6e4aa54a4a688458ac685317347
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-26 19:37:12 +0900

    Update retusr

--- retusr.sh
+++ retusr.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-cat sshlog.txt | awk 'match($0, /user.*from/){print substr($0, RSTART+5, RLENGTH-10)}'
+cat ./target/sshlog.txt | awk 'match($0, /user.*from/){print substr($0, RSTART+5, RLENGTH-10)}'
