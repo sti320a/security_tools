ba21011a636c5f46074c1aae2968da73bf9655f4
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-27 13:13:49 +0900

    Add path param

--- reqlog.sh
+++ reqlog.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 # $1 username
 # $2 hostname
-# ex) sh reqlog.sh root 123.45.678.901
+# $3 path to save
+# ex) sh reqlog.sh root 123.45.678.901 ./logfile
 
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+PATH=$3
 
-scp $1@$2:/var/log/secure ./sshlog-${TIMESTAMP}.txt
+scp $1@$2:/var/log/secure ${PATH}/sshlog-${TIMESTAMP}.txt
