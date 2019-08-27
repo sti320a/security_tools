04b2d10b560466f0fddddf7e7d83715a8ef34273
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-27 13:10:00 +0900

    Fix comment

--- reqlog.sh
+++ reqlog.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
+# $1 username
+# $2 hostname
+# ex) sh reqlog.sh root 123.45.678.901
 
-# $1 hostname
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
 
-scp $1@$2:/var/log/secure ./sshlog${TIMESTAMP}.txt
+scp $1@$2:/var/log/secure ./sshlog-${TIMESTAMP}.txt
