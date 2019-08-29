2a164bb34ab2d0b6df609742327fb890d7e26d36
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-29 12:31:40 +0900

    Add class SSHLog

--- web/app.py
+++ web/app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy()
+
+
+class SSHLog(db.Model):
+    # TODO: migrate for models dir
+    id = db.Column(db.Integer, primary_key=True)
+    from_ip = db.Column(db.String(64), nullable=False)
+    username = db.Column(db.String(64))
+    accessed_at = db.Column(db.DATETIME, default=datetime.now)
+
+    def __repr__(self):
+        return f'SSHLOG <{self.id}>'
 
 
 @app.route('/')
