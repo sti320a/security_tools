6067fd76696e218de2cfe469a353bbce59744d9a
Tomoaki Sugiayma <sti320a@gmail.com>
2019-08-29 18:36:23 +0900

    Update

--- web/app.py
+++ web/app.py
@@ -37,13 +37,14 @@ def test():
 
 def insert_log_to_db(log_file_path: str) -> bool:
     with open(log_file_path, 'r') as f:
-        # log_text = f.readlines()
-        # for line in log_text:
-        # TODO: Extract necessary text
-        #
-        # log = SSHLog(from_ip='127.0.0.1', username='root')
-        # db.session.add(log)
-        # db.session.commit()
+        log_text = f.readlines()
+        for row in log_text:
+            # TODO: Extract necessary text
+            if 'Failed' in row:
+                print(row)
+                # log = SSHLog(from_ip='127.0.0.1', username='root')
+                # db.session.add(log)
+                # db.session.commit()
         f.close()
 
     return False
