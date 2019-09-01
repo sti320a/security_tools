4d9ad7a8bbea819cc5a2da20e077a2c5372adcf8
Tomoaki Sugiayma <sti320a@gmail.com>
2019-09-01 14:39:28 +0900

    Update generate_cryptogram method

--- py/caesar_cipher.py
+++ py/caesar_cipher.py
@@ -3,8 +3,10 @@
 
 def generate_cryptogram(text: str, keynum: int) -> str:
     encrypted = ''
+    for char in text:
+        encrypted += chr(ord(char) + keynum)
     return encrypted
 
 
 if __name__ == '__main__':
-    print(generate_cryptogram('test', 2))
+    print(generate_cryptogram('test', 1))
