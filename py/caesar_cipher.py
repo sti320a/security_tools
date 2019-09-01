739536467a89da58aeb6b612e3c607a3551c5c31
Tomoaki Sugiayma <sti320a@gmail.com>
2019-09-01 14:32:03 +0900

    Add generate_cryptogram method

--- py/caesar_cipher.py
+++ py/caesar_cipher.py
@@ -1,1 +1,10 @@
 #! python3
+
+
+def generate_cryptogram(text: str, keynum: int) -> str:
+    encrypted = ''
+    return encrypted
+
+
+if __name__ == '__main__':
+    print(generate_cryptogram('test', 2))
